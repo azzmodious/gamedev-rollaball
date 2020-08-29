5c7ef6a6ff3d1f9b09294f9b695f2a1ac948a66b
azzmodious <azzmodious@gmail.com>
2020-08-28 21:31:52 -0400

    Complete Rollaball game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # gamedev-rollaball
  
+## Submit this project 
+https://learn.unity.com/tutorial/building-the-game?uv=2019.4&projectId=5f158f1bedbc2a0020e51f0d#5f319ca0edbc2a289b28b6ae
---
+++ rollaball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using TMPro;
+public class PlayerController : MonoBehaviour
+{
+    private int count;
+    public float speed = 0.0f;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject; 
+    private Rigidbody rigidBody;
+    private float movementX;
+    private float movementY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        rigidBody = GetComponent<Rigidbody>();
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+
+    // called right before physics happens
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if(count >= 6)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+    private void FixedUpdate()
+    {
+        rigidBody.AddForce(new Vector3(movementX, 0.0f, movementY)*speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Pickup")) 
+        {
+            other.gameObject.SetActive(false);
+            count += 1;
+            SetCountText();
+        }
+
+        
+        
+    }
+}
